cb2509ffcc6487a2a0130f77635d6233ff085d6c
architsharma97 <archit97sharma@gmail.com>
2016-10-19 17:39:44 +0530

    Minor changes

--- run_this.sh
+++ run_this.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#This script downloads and gets all the data preprocessed
-#Read the guide for more information
+#script downloads and preprocesses all the data
+#read the guide for more information
 
-#All Required Data is downloaded
+#download all required data
 cd ./Data/Coco
 chmod +x ./download.sh
 ./download.sh
@@ -21,7 +21,7 @@ python preprocess_train.py
 python preprocess_val.py
 
 #Optional
-#Uncomment the next line if you want to simultaneously train the model
+#uncomment the next line if you want to simultaneously train the model
 #still untested for results, so evaluation script is not available
 
 #python model_train.py
